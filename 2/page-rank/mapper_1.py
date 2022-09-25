f631b3e1367fe99caf09ee4aa60fa07f75c1099c
Nikhil Mohite <nikhilmohitelhs@gmail.com>
2022-09-25 12:22:41 +0530

    converted .py to unix format, \t for web-BerkStan.txt

--- 2/page-rank/mapper_1.py
+++ 2/page-rank/mapper_1.py
@@ -1,21 +1,21 @@
 #!/usr/bin/env python3
 
-import sys 
+import sys
 
 """ Mapper for Task 1 """
 
 for line in sys.stdin:
-    
+
     # preprocessing step - ignore lines starting with "#"
     if(line.startswith("#")):
         continue
 
     # remove whitespace
     line = line.strip()
-    src,dest = line.split(" ")
+    src, dest = line.split("\t")
 
     try:
-        print("%s\t%s"%(src,dest))
-    except Exception: 
+        print("%s\t%s" % (src, dest))
+    except Exception:
         continue
         # print(e)
--- 2/page-rank/reducer_1.py
+++ 2/page-rank/reducer_1.py
@@ -4,19 +4,21 @@ import sys
 
 """ Reducer for Task 1 """
 
+
 def read_from_mapper(w_file_path: str) -> None:
     """ Reads input from the mapper as stdin """
 
     # initialization
     node = None
     adj_list = list()
-   # open 'w' file with write permission
+
+    # open 'w' file with write permission
     with open(w_file_path, "w") as w_file:
         for line in sys.stdin:
 
             line = line.strip()
             src, dest = line.split("\t")
-            
+
             try:
                 src = int(src)
                 dest = int(dest)
@@ -40,10 +42,11 @@ def read_from_mapper(w_file_path: str) -> None:
                 node = src  # move to next node
                 # print(node,dest)
                 adj_list.append(dest)
-        
+
         # For last line
         print(f"{node}\t{adj_list}")
         w_file.write(f"{node}, 1\n")
+
 
 def main():
 
